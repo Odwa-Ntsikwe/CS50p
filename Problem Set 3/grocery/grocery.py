b5c76cfca76f5a01b83eb070020cc4217e4011c6
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-06 21:01:08 +0200

    Update grocery.py

--- Problem Set 3/grocery/grocery.py
+++ Problem Set 3/grocery/grocery.py
@@ -1,1 +1,16 @@
+groceries = {}
 
+while True:
+    try:
+        item = input().upper()
+        if item in groceries:
+            groceries[item] += 1
+        else:
+            groceries[item] = 1
+
+    except EOFError:
+        for key in sorted(groceries.keys()):
+            print(groceries[key], key)
+        break
+    except KeyError:
+        pass
