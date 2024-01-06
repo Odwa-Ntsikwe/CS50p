dc261108972a83e692fc27dfb9b6efd8b722dc1d
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-06 20:33:02 +0200

    Update faces.py

--- Problem Set 0/faces/faces.py
+++ Problem Set 0/faces/faces.py
@@ -1,1 +1,11 @@
+def main():
+    sentence = input("Enter a string: ")
+    print(converter(sentence))
 
+def converter(text):
+    text = text.replace(":)", "🙂")
+    text = text.replace(":(", "🙁")
+
+    return text
+
+main()
