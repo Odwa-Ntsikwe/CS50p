54480ec2a5543c3d38a5ef2494bda7b1300fdf3c
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-06 20:42:46 +0200

    Update extensions.py

--- Problem Set 1/extensions/extensions.py
+++ Problem Set 1/extensions/extensions.py
@@ -1,1 +1,16 @@
+file_name = str(input("File name: ")).strip()
 
+if file_name.endswith(".gif"):
+    print("image/gif")
+elif file_name.endswith(".jpg") or file_name.endswith(".jpeg"):
+    print("image/jpeg")
+elif file_name.endswith(".png"):
+    print("image/png")
+elif file_name.endswith(".pdf") or file_name.endswith(".PDF"):
+    print("application/pdf")
+elif file_name.endswith(".txt"):
+    print("text/plain")
+elif file_name.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
