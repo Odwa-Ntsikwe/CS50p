983b0dba65093fb2a48370577f9287bddc35ba49
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-06 20:53:47 +0200

    Update twttr.py

--- Problem Set 2/twttr/twttr.py
+++ Problem Set 2/twttr/twttr.py
@@ -1,1 +1,9 @@
+text = input("Input: ")
 
+vowels = ["a", "e","i", "o", "u"]
+new_text = ""
+
+for char in text:
+    if char.lower() not in vowels:
+        new_text += char
+print("Output:", new_text)
