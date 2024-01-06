672f4ebf505155b4a1688d5195b44b3d03e159a8
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-06 20:52:32 +0200

    Update coke.py

--- Problem Set 2/coke/coke.py
+++ Problem Set 2/coke/coke.py
@@ -1,1 +1,10 @@
+amount_due = 50
+coins_allowed = [25, 10, 5]
 
+while amount_due > 0:
+    print("Amount Due:", amount_due)
+    coin = int(input("Insert Coin: "))
+    if coin in coins_allowed:
+        amount_due -= coin
+change_owed = abs(amount_due)
+print("Change Owed:", change_owed)
