49063f0441ffd733953ed5a6e47d7e6b33a11913
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-06 20:51:28 +0200

    Update camel.py

--- Problem Set 2/camel/camel.py
+++ Problem Set 2/camel/camel.py
@@ -1,1 +1,15 @@
+def main():
+    variable = input("camelCase: ")
+    print("snake_case:", convert_case(variable))
 
+def convert_case(string):
+    snake_case = ""
+    for char in string:
+        if char.isupper():
+            snake_case += "_" + char.lower()
+        else:
+            snake_case += char
+    return snake_case
+
+
+main()
