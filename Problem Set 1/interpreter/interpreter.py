3a5ebfbb45a9e42162e0222ae067bb94e22c50d3
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-06 20:44:13 +0200

    Update interpreter.py

--- Problem Set 1/interpreter/interpreter.py
+++ Problem Set 1/interpreter/interpreter.py
@@ -1,1 +1,17 @@
+expression = input("Expression: ")
 
+x, y, z = expression.split(" ")
+
+a = float(x)
+b = float(z)
+
+if y == "+":
+    answer = a + b
+if y == "-":
+    answer = a - b
+if y == "*":
+    answer = a * b
+if y == "/":
+    answer = a / b
+
+print(round(answer, 1))
