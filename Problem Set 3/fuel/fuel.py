0eadf1ef8b3aa8163c75bba6cd5aea1a35b45908
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-06 20:58:29 +0200

    Update fuel.py

--- Problem Set 3/fuel/fuel.py
+++ Problem Set 3/fuel/fuel.py
@@ -1,1 +1,27 @@
+while True:
+    try:
+        frac = input("Fraction: ")
+        x,y = frac.split("/")
+        x = int(x)
+        y = int(y)
+        fuel = round((x/y)*100)
+        if x > y:
+            frac = input("Fraction: ")
 
+        if fuel <= 1:
+            print("E")
+        elif fuel > 1 and fuel <= 25:
+            print(f"{fuel}%")
+        elif fuel > 25 and fuel <= 50:
+            print(f"{fuel}%")
+        elif fuel > 50 and fuel < 99:
+            print(f"{fuel}%")
+        else:
+            print("F")
+
+    except ValueError:
+        pass
+    except ZeroDivisionError:
+        pass
+    else:
+        break
