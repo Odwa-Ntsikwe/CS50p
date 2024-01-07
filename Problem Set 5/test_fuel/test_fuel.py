f0a7485a33a55e4d0a27fa632877848817059ce0
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-07 13:53:30 +0200

    Update test_fuel.py

--- Problem Set 5/test_fuel/test_fuel.py
+++ Problem Set 5/test_fuel/test_fuel.py
@@ -1,1 +1,24 @@
+from fuel import convert, gauge
+import pytest
+
+def test_convert():
+    assert convert("3/4") == 75
+    assert convert("1/4") == 25
+    assert convert("4/4") == 100
+    assert convert("0/4") == 0
+
+def test_gauge():
+    assert gauge(75) == "75%"
+    assert gauge(25) == "25%"
+    assert gauge(99) == "F"
+    assert gauge(1) == "E"
+
+
+def test_zero_division_error():
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
+
+def test_value_error():
+    with pytest.raises(ValueError):
+        convert("three/four")
 
