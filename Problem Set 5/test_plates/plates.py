5ae0e1ac7b90eb06c0942bc0bb7ae8bccbdb321b
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-07 13:52:15 +0200

    Update plates.py

--- Problem Set 5/test_plates/plates.py
+++ Problem Set 5/test_plates/plates.py
@@ -1,1 +1,28 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
+
+def is_valid(s):
+    if len(s) <= 6 and len(s) >= 2 and s[0:2].isalpha() and s.isalnum():
+        for i in range(len(s)):
+            if s[i].isdigit():
+                if  s[i:].isdigit() and s[i] != "0":
+                    return True
+                else:
+                    return False
+        return True
+    return False
+
+    for c in s:
+        if c in [".", ",", "!", "?"]:
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    main()
