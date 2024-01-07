a3ef04914503ce2978eb0ffc1ac515eea4c708ad
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-07 13:54:12 +0200

    Update fuel.py

--- Problem Set 5/test_fuel/fuel.py
+++ Problem Set 5/test_fuel/fuel.py
@@ -1,1 +1,36 @@
+def main():
+    frac = input("Fraction: ")
+    converted_frac = convert(frac)
+    print(gauge(converted_frac))
 
+
+def convert(fraction):
+    try:
+        x,y = fraction.split("/")
+        x = int(x)
+        y = int(y)
+
+        if x/y <= 1:
+            return int(round((x/y)*100))
+        else:
+            fraction = input("Fraction: ")
+            pass
+    except ValueError:
+        raise ValueError
+    except ZeroDivisionError:
+        raise ZeroDivisionError
+
+
+
+
+
+def gauge(percentage):
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
+    return f"{percentage}%"
+
+
+if __name__ == "__main__":
+    main()
