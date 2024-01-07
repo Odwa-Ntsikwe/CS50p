36fbb1ddb1a39c7e4455d26249d49939c4a34fd5
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-07 13:38:29 +0200

    Update adieu.py

--- Problem Set 4/adieu/adieu.py
+++ Problem Set 4/adieu/adieu.py
@@ -1,1 +1,13 @@
+import inflect
 
+p = inflect.engine()
+names = []
+while True:
+    try:
+        name = input("Name: ")
+        names.append(name)
+
+    except EOFError:
+        break
+
+print("Adieu, adieu, to", p.join(names))
