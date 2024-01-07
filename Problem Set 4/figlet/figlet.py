8b591344641db520b72625b16960d191f521cc82
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-07 13:37:12 +0200

    Update figlet.py

--- Problem Set 4/figlet/figlet.py
+++ Problem Set 4/figlet/figlet.py
@@ -1,1 +1,19 @@
+import sys
+import random
+from pyfiglet import Figlet
 
+figlet = Figlet()
+fonts = figlet.getFonts()
+args = ["-f", "--font"]
+
+if len(sys.argv) < 2:
+    font = random.choice(figlet.getFonts())
+    figlet.setFont(fonts = font)
+elif len(sys.argv) > 2 and sys.argv[1] in args and sys.argv[2] in fonts:
+    figlet.setFont(font = sys.argv[2])
+else:
+    sys.exit("Invalid usage")
+
+text = input("Input: ")
+
+print("Output:", figlet.renderText(text))
