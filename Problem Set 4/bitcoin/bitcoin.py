5850799ddb919aa7479ca0ed9e9399e70ebe222e
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-07 13:42:05 +0200

    Update bitcoin.py

--- Problem Set 4/bitcoin/bitcoin.py
+++ Problem Set 4/bitcoin/bitcoin.py
@@ -1,1 +1,20 @@
+import requests
+import sys
+import json
 
+if len(sys.argv) == 2:
+    try:
+        n = float(sys.argv[1])
+    except:
+        sys.exit("Command-line argument is not a number")
+else:
+    sys.exit("Missing command-line argument")
+
+try:
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    result = response.json()
+    bitcoin_amount = result["bpi"]["USD"]["rate_float"]
+    total_amount = bitcoin_amount * n
+    print(f"${total_amount:,.4f}")
+except requests.RequestException:
+    pass
