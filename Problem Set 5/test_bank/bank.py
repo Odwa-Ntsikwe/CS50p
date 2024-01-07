aa3c29539cf900502d7ac824a27d98b0a40e414f
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-07 13:50:01 +0200

    Update bank.py

--- Problem Set 5/test_bank/bank.py
+++ Problem Set 5/test_bank/bank.py
@@ -1,1 +1,17 @@
+def main():
+    greeting = str(input("Greeting: ")).lower().strip()
+    output = value(greeting)
+    print(f"${output})
 
+
+def value(greeting):
+    if greeting.startswith("hello"):
+        return 0
+    elif greeting.startswith("hello"):
+        return 20
+    else:
+        return 100
+
+
+if __name__ == "__main__":
+    main()
