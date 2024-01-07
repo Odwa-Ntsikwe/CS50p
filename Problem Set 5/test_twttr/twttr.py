9c05aca0fb68c58394f6c8163be9a7a16ab01f3f
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-07 13:47:17 +0200

    Update twttr.py

--- Problem Set 5/test_twttr/twttr.py
+++ Problem Set 5/test_twttr/twttr.py
@@ -1,1 +1,20 @@
+def main():
+    text = input("Input: ")
+    output = shorten(text)
+    print(f"Output: {output}")
+
+
+def shorten(word):
+    vowels = ["a", "e","i", "o", "u"]
+    new_text = ""
+
+    for char in word:
+        if char.lower() not in vowels:
+            new_text += char
+    return new_text
+
+
+
+if __name__ == "__main__":
+    main()
 
