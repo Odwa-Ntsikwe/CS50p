545e028c24706c881425c3e3bf94025772a6c5f5
Odwa Ntsikwe <116360204+Odwa-Ntsikwe@users.noreply.github.com>
2024-01-07 13:51:29 +0200

    Update test_plates.py

--- Problem Set 5/test_plates/test_plates.py
+++ Problem Set 5/test_plates/test_plates.py
@@ -1,1 +1,27 @@
+from plates import is_valid
 
+def test_start_letters():
+    assert is_valid("CS") == True
+    assert is_valid("CS50") == True
+    assert is_valid("H") == False
+    assert is_valid("@CSP") == False
+    assert is_valid("0sc") == False
+    assert is_valid("50") == False
+    assert is_valid("11cs50") == False
+
+def test_middle_num():
+    assert is_valid("AAA222") == True
+    assert is_valid("AAA22A") == False
+    assert is_valid("CS05") == False
+
+
+def test_punctuation():
+    assert is_valid("PI3.14") == False
+    assert is_valid("CS50!") == False
+
+def test_char_length():
+    assert is_valid("OUTATIME") == False
+    assert is_valid("Hello,  world") == False
+    assert is_valid("AAA222") == True
+    assert is_valid("CS") == True
+    assert is_valid(" H") == False
